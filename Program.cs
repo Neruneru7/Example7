198869d5b119a26437b58efd7f22262ad05f5fdc
WIN_NT\x16j002 <x16j002@chiba-fjb.ac.jp>
2017-05-12 12:19:52 +0900

    作成途中

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Example7
     {
         static void Main(string[] args)
         {
+            sbyte valueSByte = -100;
+            byte valueByte = 100;
+            short valudShor = -20000;
+            ushort valueUShort = 50000;
+            int valueInt = -1000000000;
+            uint valueUInt = 4000000000;
+            long valueLong = -80000000000;
+            ulong valueULong = 9000000000000000000;
+
+            float valueFloat = 3.1415F;
+            double valueDouble = 3.1415926535;
+
+            char valueChar = 'A';
+            string valueString = "C# PROGRAM";
         }
     }
 }
